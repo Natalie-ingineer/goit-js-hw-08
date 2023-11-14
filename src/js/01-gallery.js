137e8bbf50bc2955284fce212e33ed17e306c4e7
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-11-14 14:51:47 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,3 +4,31 @@ import { galleryItems } from './gallery-items';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+const container = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+
+`
+  )
+  .join('');
+
+container.insertAdjacentHTML('beforeend', markup);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  sourceAtt: 'href',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
