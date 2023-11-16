8273173af810d66e454fb7962852a72779e65f31
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-11-16 12:19:24 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,44 +2,50 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const textarea = document.querySelector('textarea');
+const inputEmail = document.querySelector('input');
 
-const STORAGE_KEY = 'feedback';
+const STORAGE_KEY = 'feedback-form-state';
 
 const formData = {};
 
 populateTextarea();
 
-form.addEventListener('submit', onFormSabmit);
-textarea.addEventListener('input', throttle(onTextareaInput, 500));
+// form.addEventListener('submit', onFormSabmit);
+// textarea.addEventListener('input', throttle(onTextareaInput, 500));
+form.addEventListener('input', throttle(onTextareaInput, 500));
 
 form.addEventListener('input', evt => {
-  //   console.log(evt.target.name);
+  //   console.log(evt.target.name); - ім'я інпуту (email,message)
   //   console.log(evt.target.value);
+
+  // для того щоб вивести об'єкт з назвою (ключем) та значенням
 
   formData[evt.target.name] = evt.target.value;
   console.log(formData);
 });
 
-function onFormSabmit(event) {
-  event.preventDefault();
+// function onFormSabmit(event) {
+// event.preventDefault();
 
-  console.log('Відправили форму!');
+// console.log('Відправили форму!');
 
-  event.target.reset();
-  localStorage.removeItem(STORAGE_KEY);
-}
+// event.target.reset();
+// localStorage.removeItem(STORAGE_KEY);
+// }
 
 function onTextareaInput(event) {
-  const valueInput = event.target.value;
-  console.log(valueInput);
-  localStorage.setItem(STORAGE_KEY, valueInput);
+  const strValueInput = JSON.stringify(formData);
+  console.log(strValueInput);
+  localStorage.setItem(STORAGE_KEY, strValueInput);
 }
 
 function populateTextarea() {
   const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const parseMessage = JSON.parse(savedMessage);
+  console.log(parseMessage);
 
-  if (savedMessage) {
-    console.log(savedMessage);
-    textarea.value = savedMessage;
+  if (parseMessage) {
+    console.log(parseMessage);
+    form.value = parseMessage;
   }
 }
