2d9ef070581435c0e9afa8b24f09c02655bc2e0b
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-11-17 17:35:34 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,7 @@
-// import throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 console.log(Player);
 
 const iframeVideo = document.querySelector('iframe');
@@ -17,21 +18,18 @@ const onPlay = function (data) {
     percent: 0.049;
     seconds: 3.034;
   }
+  const valueInput = data.seconds;
+  console.log(valueInput);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(valueInput));
 };
 
-const valueInput = iframeVideo.setCurrentTime;
-console.log(valueInput);
-localStorage.setItem('videoplayer-current-time', JSON.stringify(valueInput));
-
-iframeVideo.addEventListener('timeupdate', onPlay);
+iframeVideo.addEventListener('timeupdate', throttle(onPlay, 1000));
 
 player.on('timeupdate', onPlay);
 
 player
-  .setCurrentTime(30.456)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {})
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
@@ -43,72 +41,3 @@ player
         break;
     }
   });
-
-// function onTextareaInput(event) {
-//   const valueInput = event.target.value;
-//   console.log(valueInput);
-//   localStorage.setItem(STORAGE_KEY, valueInput);
-// }
-
-// localStorage.setItem('videoplayer-current-time', target.value);
-
-// const player = new Player('handstick', {
-// id: iframe.id,
-// width: 640,
-// });
-
-// player.on('play', function () {
-// console.log('played the video!');
-// });
-
-// localStorage.setItem(
-// 'videoplayer-current-time',
-// JSON.stringify({ name: 'Mango', age: 22 })
-// );
-
-// iframe.getAttribute("id");
-// console.log(iframe("id"));
-
-// const form = document.querySelector('.feedback-form');
-// const textarea = document.querySelector('textarea');
-
-// const STORAGE_KEY = 'feedback';
-
-// populateTextarea();
-
-// form.addEventListener('submit', onFormSabmit);
-// textarea.addEventListener('input', throttle(onTextareaInput, 500));
-
-// function onFormSabmit(event) {
-//   event.preventDefault();
-
-//   console.log('Відправили форму!');
-
-//   event.target.reset();
-//   localStorage.removeItem(STORAGE_KEY);
-// }
-
-// function onTextareaInput(event) {
-//   const valueInput = event.target.value;
-//   console.log(valueInput);
-//   localStorage.setItem(STORAGE_KEY, valueInput);
-// }
-
-// function populateTextarea() {
-//   const savedMessage = localStorage.getItem(STORAGE_KEY);
-
-//   if (savedMessage) {
-//     console.log(savedMessage);
-//     textarea.value = savedMessage;
-//   }
-// }
-
-// const player = new Player('handstick', {
-//   id: iframe.id,
-//   width: 640,
-// });
-
-// player.on('play', function () {
-// console.log('played the video!');
-// });
-// let currentTime =
